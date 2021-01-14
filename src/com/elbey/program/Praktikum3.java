eb2052f2756c3e5f7f2ded366432599760ead113
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 21:10:34 +0700

    Project Ketujuh

---
+++ src/com/elbey/program/Praktikum3.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.elbey.program;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Praktikum3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Mendeklarasikan Variabel
+       int sp;
+       //Perulangan
+       for(sp=0; sp<2; sp++){
+           System.out.println("");
+       }//spasi
+       int zx = 4;
+       for(int z=0; z<5; z++){
+           System.out.print("@");
+       }
+       //Perintah untuk menghasilkan pola @
+        System.out.println("");
+        System.out.println("   @");
+        System.out.println("  @");
+        System.out.println(" @");
+        for(int z=0; z<5; z++){
+            System.out.print("@");
+        }
+        //Untuk menghasilkan baris baru
+        System.out.println("");
+        
+        
+            
+        
+        
+            
+        
+    }
+    
+}
